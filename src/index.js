78a7269a462929479f9ba2835e9485fcea2914e7
pumpedlunch <88350639+pumpedlunch@users.noreply.github.com>
2022-09-23 16:00:24 -0700

    fix css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import List from './components/list'
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
