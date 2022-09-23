c72cf8c8881dfc7f379b6dec66399a2a61e786a5
pumpedlunch <88350639+pumpedlunch@users.noreply.github.com>
2022-09-23 15:38:15 -0700

    update

--- src/components/list.js
+++ src/components/list.js
@@ -1,16 +1,18 @@
 import React, { useState, createRef, useEffect, useRef } from "react";
 import { ethers } from "ethers";
 import MetaMaskButton from "./metamaskButton";
-import AddressModal from './addressModal'
-import ListModal from './listModal'
+import AddressModal from "./addressModal";
+import ListModal from "./listModal";
 
+import LOGO from "./decentralist.png";
 const PROXY_ABI =
   require("../artifacts/contracts/decentralist.sol/Decentralist.json").abi;
 const FACTORY_ABI =
   require("../artifacts/contracts/DecentralistProxyFactory.sol/DecentralistProxyFactory.json").abi;
 const WETH_ABI = require("../public/WETH_ABI.json");
-const FACTORY_ADDRESS = "0x4B88f76057f06e03B7337a2E033d952f12e0A3Cf";
+const FACTORY_ADDRESS = "0xF44b59Ff544ce831A8B49331047Fc0a4dfa3B3B3";
 const WETH_ADDRESS = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6"; // Goerli
+
 //require("dotenv").config();
 
 export default function List() {
@@ -45,79 +47,79 @@ export default function List() {
 
   const handleAddressInputChange = (event) => {
     setAddressInput(event.target.value);
-  }
+  };
 
   const handleSubmitApproval = (event) => {
     event.preventDefault();
     approveTransfer();
-  }
+  };
 
   const handleSubmitAddressInput = (event) => {
     event.preventDefault();
-    if(isAdd) {
+    if (isAdd) {
       addAddress();
     } else {
       removeAddress();
     }
     closeAddressModal();
-  }
+  };
 
   const openAddressModal = (add) => {
     setIsAdd(add);
     setAddressModalIsOpen(true);
-  }
+  };
 
   const closeAddressModal = () => {
     setAddressModalIsOpen(false);
-  }
+  };
 
   // List modal functions
 
   const handleAncillaryArgChange = (event) => {
     setAncillaryArg(event.target.value);
-  }
-  
+  };
+
   const handleTitleArgChange = (event) => {
     setTitleArg(event.target.value);
-  }
-  
+  };
+
   const handleLivenessPeriodArgChange = (event) => {
     setLivenessPeriodArg(event.target.value);
-  }
+  };
 
   const handleBondAmountArgChange = (event) => {
     setBondAmountArg(event.target.value);
-  }
-  
+  };
+
   const handleAddRewardArgChange = (event) => {
     setAddRewardArg(event.target.value);
-  }
-  
+  };
+
   const handleRemoveRewardArgChange = (event) => {
     setRemoveRewardArg(event.target.value);
-  }
+  };
 
   const handleSubmitList = (event) => {
     event.preventDefault();
-    createList()
-    console.log('ancillaryArg', ancillaryArg)
-    console.log('titleArg', titleArg)
-    console.log('livenessPeriodArg', livenessPeriodArg)
-    console.log('bondAmountArg', bondAmountArg)
-    console.log('addRewardArg', addRewardArg)
-    console.log('removeRewardArg', removeRewardArg)
-    closeListModal()
-  }
+    createList();
+    console.log("ancillaryArg", ancillaryArg);
+    console.log("titleArg", titleArg);
+    console.log("livenessPeriodArg", livenessPeriodArg);
+    console.log("bondAmountArg", bondAmountArg);
+    console.log("addRewardArg", addRewardArg);
+    console.log("removeRewardArg", removeRewardArg);
+    closeListModal();
+  };
 
   const openListModal = () => {
     setListModalIsOpen(true);
-  }
+  };
 
   const closeListModal = () => {
     setListModalIsOpen(false);
-  }
+  };
 
-   useEffect(() => {
+  useEffect(() => {
     async function getProxies() {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const contract = new ethers.Contract(
@@ -162,6 +164,7 @@ export default function List() {
     );
 
     await Promise.all(promises).then((values) => {
+      console.log(values[0]);
       setAddresses(values[0]);
       setTitle(values[1]);
       setBondAmount(values[2].toNumber());
@@ -210,10 +213,10 @@ export default function List() {
     const createListTx = await contract.createNewDecentralist(
       ethers.utils.hexlify(ethers.utils.toUtf8Bytes(ancillaryArg)),
       titleArg,
-      livenessPeriodArg * 60 * 60,
-      ethers.utils.parseEther(bondAmountArg),
-      ethers.utils.parseEther(addRewardArg),
-      ethers.utils.parseEther(removeRewardArg)
+      livenessPeriodArg,
+      bondAmountArg,
+      addRewardArg,
+      removeRewardArg
     );
   };
 
@@ -225,164 +228,185 @@ export default function List() {
   };
 
   return (
-    <div>
-      <MetaMaskButton></MetaMaskButton>
-      <AddressModal 
-        isOpen={addressModalIsOpen} 
-        closeModal={closeAddressModal} 
-        isAdd={isAdd} 
-        handleAddressInputChange={handleAddressInputChange} 
-        handleSubmitAddressInput={handleSubmitAddressInput} 
-        handleSubmitApproval={handleSubmitApproval}  
+    <div className="bg-gradient-to-t from-gray-200 h-full min-h-[750px]">
+      <AddressModal
+        isOpen={addressModalIsOpen}
+        closeModal={closeAddressModal}
+        isAdd={isAdd}
+        handleAddressInputChange={handleAddressInputChange}
+        handleSubmitAddressInput={handleSubmitAddressInput}
+        handleSubmitApproval={handleSubmitApproval}
       />
-      <ListModal 
-        isOpen={listModalIsOpen} 
-        closeModal={closeListModal} 
-        handleAncillaryArgChange={handleAncillaryArgChange} 
-        handleTitleArgChange={handleTitleArgChange} 
-        handleLivenessPeriodArgChange={handleLivenessPeriodArgChange}  
-        handleBondAmountArgChange={handleBondAmountArgChange}  
-        handleAddRewardArgChange={handleAddRewardArgChange}  
-        handleRemoveRewardArgChange={handleRemoveRewardArgChange}  
+      <ListModal
+        isOpen={listModalIsOpen}
+        closeModal={closeListModal}
+        handleAncillaryArgChange={handleAncillaryArgChange}
+        handleTitleArgChange={handleTitleArgChange}
+        handleLivenessPeriodArgChange={handleLivenessPeriodArgChange}
+        handleBondAmountArgChange={handleBondAmountArgChange}
+        handleAddRewardArgChange={handleAddRewardArgChange}
+        handleRemoveRewardArgChange={handleRemoveRewardArgChange}
         handleSubmitList={handleSubmitList}
         closeAddressModal={closeAddressModal}
       />
-      <form>
-        <label>
-          Select a List:
-          <select className="border border-black" onChange={changeList}>
-            <option value="none" selected disabled hidden>
-              Select an Option
-            </option>
-            {proxyTitles.map((title) => (
-              <option value={title}>{title}</option>
-            ))}
-          </select>
-        </label>
-      </form>
-      <div className="flex border border-black justify-between">
-        <div className="flex flex-col ">
-          {/* <h3 className="font-bold">Title: {title}</h3> */}
-          <p>Contract Address:</p>
+      <div className="relative px-20">
+        <div className="flex justify-between py-3 border-b-2 border-black z-30">
+          <div className="flex flex-w">
+            <img src={LOGO} alt="logo" width="60" height="25" />
+            <div className="ml-2">
+              <p className="font-bold font-sans text-3xl text-indigo-900 mt-2">
+                decentraList
+              </p>
+              <p className="text-xs font-bold">
+                customizable, decentralized, onchain, address lists
+              </p>
+            </div>
+          </div>
+          <MetaMaskButton />
         </div>
-        <a
-          className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
-          href={`https://goerli.etherscan.io/address/${currentProxy}`}
-        >
-          {currentProxy}
-        </a>
-        <div className="flex flex-col items-center justify-center space-y-2 p-3">
-        <p className="bg-gray-500">Number of Addresses on List: {addresses.length}</p>
-        <p className="bg-gray-300">{addresses.length} addresses </p>
+        <div className="flex flew-w justify-between items-center">
+          <form className="my-3">
+            <label>
+              <select
+                className="bg-slate-100 shadow px-2 py-2 rounded-md"
+                onChange={changeList}
+              >
+                <option
+                  className="p-5 font-bold"
+                  value="none"
+                  selected
+                  disabled
+                  hidden
+                >
+                  Select a List
+                </option>
+                {proxyTitles.map((title) => (
+                  <option value={title}>{title}</option>
+                ))}
+              </select>
+            </label>
+          </form>
+          <div className="my-3">
+            <button
+              type="button"
+              className="text-sm font-bold px-3 py-3 items-center rounded-md bg-[#abdbe3]  text-xs font-bold shadow-md hover:bg-sky-700 hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              onClick={openListModal}
+            >
+              Create List
+            </button>
+          </div>
         </div>
-        <div className="flex flex-col items-center justify-center space-y-2 p-3">
-        <p className="bg-gray-500">Bond Amount:  </p>
-        <p className="bg-gray-300">{bondAmount} bondAmount WETH wei</p>
-        </div>
-        <div className="flex flex-col items-center justify-center space-y-2 p-3">
-        <p className="bg-gray-500">Reward / Address Added: </p>
-        <p className="bg-gray-300">{addReward} WETH wei</p>
-        </div>
-        <div className="flex flex-col items-center justify-center space-y-2 p-3">
-        <p className="bg-gray-500">Reward / Address Removed: </p>
-        <p className="bg-gray-300">{removeReward} WETH wei </p>
-        </div>
-        <div className="flex flex-col items-center justify-center space-y-2 p-3">
-        <p className="bg-gray-500">Oracle Liveness Period: {livenessPeriod} </p>
-        <p className="bg-gray-300">{livenessPeriod} seconds</p>
-        </div>
-        <div className="flex flex-col items-center justify-center space-y-2 p-3">
-        <p className="bg-gray-500">Ancillary Data: </p>
-        <p className="bg-gray-300">{fixedAncillaryData} seconds</p>
-        </div>
-      </div>
-      <div>
-        <button className="bg-green-500" onClick={() => openAddressModal(true)}>Add Addresses (opens modal)</button>
-        <button className="bg-red-500" onClick={() => openAddressModal(false)}>Remove Addresses (opens modal)</button>
-      </div>
-      <list>
-        {addresses.map((address) => (
-          <ul>{address}, LIST</ul>
-        ))}
-      </list>
 
-      {/* -------------- ADD/REMOVE ADDRESS ----------------- */}
-      <div className="mt-8 border border-black">
-        <div className="flex">
-          <p>Bond Amount: {bondAmount / 10e18} WETH</p>
-          <button className="bg-yellow-500" onClick={approveTransfer}>
-            Approve Transfer of Bond
-          </button>
+        <div className="flex rounded-lg bg-white px-4 py-2 shadow sm:p-2">
+          <div className="flex flex-col ">
+            <p className="font-medium text-gray-500">Contract Address:</p>
+          </div>
+          <a
+            className="cursor-pointer pl-1 font-semibold"
+            href={`https://goerli.etherscan.io/address/${currentProxy}`}
+          >
+            {currentProxy}
+          </a>
         </div>
         <div>
-          <input
-            placeholder="0x or 0x,0x,0x..."
-            className="border border-black w-[360px]"
-            onChange={(e) => {
-              setAddressInput(e.target.value);
-            }}
-          ></input>
-          <button className="bg-green-500" onClick={addAddress}>
-            Add Address
-          </button>
-          <button className="bg-red-500" onClick={removeAddress}>
-            Remove Address
-          </button>
+          <dl className="mt-5 grid grid-cols-1 gap-10 sm:grid-cols-5 text-center">
+            <div className="overflow-hidden rounded-lg bg-white px-4 py-2 shadow sm:p-2">
+              <dt className="truncate text-sm font-medium text-gray-500">
+                List Length
+              </dt>
+              <dd className="mt-1 text-xl font-semibold tracking-tight text-gray-800">
+                {addresses.length}
+              </dd>
+            </div>
+            <div className="overflow-hidden rounded-lg bg-white px-4 py-2 shadow sm:p-2">
+              <dt className="truncate text-sm font-medium text-gray-500">
+                Oracle Bond
+              </dt>
+              <dd className="mt-1 text-xl font-semibold tracking-tight text-gray-800">
+                {bondAmount} WETH wei
+              </dd>
+            </div>
+            <div className="overflow-hidden rounded-lg bg-white px-4 py-2 shadow sm:p-2">
+              <dt className="truncate text-sm font-medium text-gray-500">
+                Oracle Liveness{" "}
+              </dt>
+              <dd className="mt-1 text-xl font-semibold tracking-tight text-gray-800">
+                {livenessPeriod / 60} min
+              </dd>
+            </div>
+            <div className="overflow-hidden rounded-lg bg-white px-4 py-2 shadow sm:p-2">
+              <dt className="truncate text-sm font-medium text-gray-500">
+                Reward / Add
+              </dt>
+              <dd className="mt-1 text-xl font-semibold tracking-tight text-gray-800">
+                {addReward} WETH wei
+              </dd>
+            </div>
+            <div className="overflow-hidden rounded-lg bg-white px-4 py-2 shadow sm:p-2">
+              <dt className="truncate text-sm font-medium text-gray-500">
+                Reward / Removal
+              </dt>
+              <dd className="mt-1 text-xl font-semibold tracking-tight text-gray-800">
+                {removeReward} WETH wei
+              </dd>
+            </div>
+          </dl>
+          <dl className="mt-5 flex items-center justify-left">
+            <div className="overflow-hidden rounded-lg bg-white px-4 shadow sm:p-4 text-left">
+              <dt className="truncate text-sm font-medium text-gray-500">
+                Ancillary Data:
+              </dt>
+              <dd className="mt-1 text tracking-tight text-black">
+                {fixedAncillaryData}
+              </dd>
+            </div>
+          </dl>
         </div>
-      </div>
-      {/* -------------- CREATE LIST ----------------- */}
-       <div className="mt-8 border border-black">
-        {/*<p>Ancillary Data:</p>
-        <input
-          placeholder="Text..."
-          className="border border-black w-[360px]"
-          onChange={(e) => {
-            setAncillaryArg(e.target.value);
-          }}
-        ></input>
-        <p>Title:</p>
-        <input
-          placeholder="Text..."
-          className="border border-black w-[360px]"
-          onChange={(e) => {
-            setTitleArg(e.target.value);
-          }}
-        ></input>
-        <p>Liveness Period:</p>
-        <input
-          placeholder="seconds"
-          className="border border-black w-[360px]"
-          onChange={(e) => {
-            setLivenessPeriodArg(e.target.value);
-          }}
-        ></input>
-        <p>Bond Amount:</p>
-        <input
-          placeholder="WETH"
-          className="border border-black w-[360px]"
-          onChange={(e) => {
-            setBondAmountArg(e.target.value);
-          }}
-        ></input>
-        <p>Add Reward:</p>
-        <input
-          placeholder="WETH"
-          className="border border-black w-[360px]"
-          onChange={(e) => {
-            setAddRewardArg(e.target.value);
-          }}
-        ></input>
-        <p>Remove Reward:</p>
-        <input
-          placeholder="WETH"
-          className="border border-black w-[360px]"
-          onChange={(e) => {
-            setRemoveRewardArg(e.target.value);
-          }}
-        ></input> */}
-        <button className="bg-green-500" onClick={openListModal}>
-          Open Create List Modal
-        </button>
+        <div className="flex flex-w justify-between my-2">
+          <div>
+            <dt className="truncate text-xl font-medium mt-8">Addresses:</dt>
+          </div>
+          <div className="my-2">
+            <button
+              type="button"
+              className="items-center rounded-md bg-[#ace4aa] p-3 text-sm font-bold shadow-md hover:bg-sky-700 hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              onClick={() => openAddressModal(true)}
+            >
+              Add Addresses
+            </button>
+            <button
+              type="button"
+              className="ml-2 items-center rounded-md bg-[#e4aeaa] p-3 text-sm font-bold shadow-md hover:bg-sky-700 hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              onClick={() => openAddressModal(false)}
+            >
+              Remove Addresses
+            </button>
+          </div>
+        </div>
+        <div className="overflow-hidden rounded-lg bg-black px-4 shadow text-left grid grid-cols-2">
+          <div>
+            <ul className="py-2">
+              {addresses
+                .slice(0, Math.ceil(addresses.length / 2))
+                .map((address) => (
+                  <li key={address} className="font-mono text-white">
+                    {address},
+                  </li>
+                ))}
+            </ul>
+          </div>
+          <div>
+            <ul className="py-2">
+              {addresses
+                .slice(Math.ceil(addresses.length / 2))
+                .map((address) => (
+                  <li key={address} className="font-mono text-white">
+                    {address},
+                  </li>
+                ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
