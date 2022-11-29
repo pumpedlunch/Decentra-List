7131a84cf95703223d95183f34c15ee70f3969ac
pumpedlunch <88350639+pumpedlunch@users.noreply.github.com>
2022-11-29 12:28:05 -0800

    update front end

--- src/components/metamaskButton.js
+++ src/components/metamaskButton.js
@@ -1,74 +1,44 @@
 import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 
-export default function MetaMaskButton() {
-  const [userAddress, setUserAddress] = useState("");
-
-  useEffect(() => {
-    checkIfWalletIsConnected();
-  }, []);
-
-  useEffect(() => {
-    // connect();
-  }, [userAddress]);
-
-  const connect = async () => {
-    console.log("connecting");
-    if (!window.ethereum) {
-      alert("Get MetaMask!");
-      return;
-    }
-
-    const account = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-
-    setUserAddress(account);
-
-    const chainId = await window.ethereum.request({ method: "eth_chainId" });
-    console.log(chainId);
-    if (chainId !== '0x5'){
-      alert("Please switch network to Goerli");
-    }
-  };
-
-  async function checkIfWalletIsConnected() {
-    console.log("checking");
-    if (window.ethereum) {
-      const accounts = await window.ethereum.request({
-        method: "eth_accounts",
-      });
-      const chainId = await window.ethereum.request({ method: "eth_chainId" });
-      if (chainId !== '0x5'){
-        alert("Please switch network to Goerli");
-      }
-
-      if (accounts.length > 0) {
-        const account = accounts[0];
-        setUserAddress(account);
-        return;
-      }
-    }
-  }
+export default function MetaMaskButton({
+  network, userAddress, connect
+}) {
+ 
 
   return (
     <>
-      {userAddress ? (
-        <div className="flex flex-col items-center text-xl justify-center">
-          <button className="bg-[#ace4aa] p-2 mt-2 rounded-md w-32 text-sm font-bold text-center float-right">
-            Connected
-            <p className="text-sm font-bold truncate max-w-[150px] ">
-              {userAddress}
-            </p>
-          </button>
-        </div>
-      ) : (
+      {network === null? (
+        <div>
         <button
-          className="bg-[#abdbe3] p-2 mt-2 rounded-md w-32 text-sm font-bold text-center float-right"
-          onClick={() => connect()}
-        >
-          Connect
+          className="bg-red-300 p-1 mt-2 rounded-md w-40 h-12 border border-4 border-red-500"
+          >
+          <p className="text-sm font-bold">Change to Mainnet or Goerli</p>
         </button>
+      </div>
+      ) : (
+        <>{userAddress ? (
+          <div className="flex items-right text-xl mt-2 justify-end content-center">
+            <button className="bg-slate-300 rounded-md w-40 h-12 align-middle h-14 text-sm font-bold text-center float-right">
+              <p className="text-sm font-bold truncate max-w-[155px] ">
+                Connected
+              </p>
+              <p className="text-sm font-bold truncate max-w-[155px] pl-2">
+                {network} - {userAddress}
+              </p>
+            </button>
+          </div>
+        ) : (
+          <div>
+            <button
+              className="bg-blue-300 p-2 mt-2 rounded-md w-40 h-12 align-middle text-sm font-bold text-center float-right"
+              onClick={() => connect()}
+              >
+              Connect
+            </button>
+          </div>
+        )}
+      </>
       )}
     </>
   );
