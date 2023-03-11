75712d0d916dbb6c396a8fd84d5c60cd39df2619
pumpedlunch <88350639+pumpedlunch@users.noreply.github.com>
2023-03-10 16:35:13 -0800

    update readme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -166,7 +166,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d79051b795c445ae"; }
+/******/ 		__webpack_require__.h = function() { return "6a471f2713a4c9a6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
